1c7d8eac150b358cb4285df84bbe9fea0687039e
Nuwan <bnuwan53@gmail.com>
2019-03-18 10:05:46 +0530

    add_guest list page

---
+++ app/src/main/java/unme/app/com/ume/guest_add.java
@@ -0,0 +1,13 @@
+package unme.app.com.ume;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class guest_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_add);
+    }
+}
